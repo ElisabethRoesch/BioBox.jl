aa9cd8b72c33bd11f73a50709d411ac2c717eb38
LislPisl <eroesch@student.unimelb.edu.au>
2020-07-28 23:41:58 +1000

    polish

--- test/TS/TS_model.jl
+++ test/TS/TS_model.jl
@@ -6,7 +6,7 @@ using Plots, DifferentialEquations
 # α1 and α2 denote the effective rates of synthesis of repressors 1 and 2
 # β and γ are the cooperativity of repression of promoter 1 and of repressor 2
 
-# derivative
+# derivative as function
 function dudt(states, ps, t)
     alpha1,beta,alpha2, gamma =ps
     u,v=states
@@ -15,14 +15,14 @@ function dudt(states, ps, t)
     return [du,dv]
 end
 # test setting
-u0=[3.,2.9]
-ps=[100.,2.,100.,2.] # from Supplementary material
-tspan = (0.0,100.0)
+u0=[3., 2.9]
+ps=[100., 2., 100., 2.] # from Supplementary material (BMC Syst Biol 2016 Leon)
+tspan = (0.0, 100.0)
 prob =ODEProblem(dudt, u0, tspan, ps)
 
 # solve example
 sol = Array(solve(prob))
 
-# visualise sol
+# visualise solution
 plot(sol[1,:], label = "U")
 plot!(sol[2,:], label = "V")
