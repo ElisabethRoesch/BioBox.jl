13a1652f69cab74253e89f45e05c03bb6486e95c
LislPisl <eroesch@student.unimelb.edu.au>
2020-09-16 15:20:20 +1000

    tidy

--- test/dynamic/tester1.jl
+++ test/dynamic/tester1.jl
@@ -1,6 +1,5 @@
 # Do same as static but with adding ODE sol each time new.
 using DiffEqFlux, OrdinaryDiffEq, Flux, Optim, Plots
-
 u0_network = [0.001;0.001]
 u0 = Float32[2.; 0.]
 datasize = 30
@@ -34,14 +33,13 @@ scatter!(t,knownPartData[1,:],label="known part")
 scatter!(t,knownPartData[2,:],label="known part")
 #savefig("plots/prior4.pdf")
 dudt2 = FastChain((x,p) -> x.^3, # Remove this line?
-            FastDense(2,50,tanh),
-            FastDense(50,2))
-n_ode = NeuralODE(dudt2,tspan,Tsit5(),saveat=t)
-
+            FastDense(2, 50, tanh),
+            FastDense(50, 2))
+n_ode = NeuralODE(dudt2, tspan, Tsit5(), saveat = t)
 function predict_n_ode(p)
   current_network = n_ode.model # get current NN of Neural ODE
   print(current_network(u0_network, n_ode.p)) # get gradient prediction for a U of one time point (here u0_network)
-  test_call = knownPartODEfunc([0.0,0.0],[0.,0.],1,1) # test cope of knownPartODEfunc
+  test_call = knownPartODEfunc([0.0, 0.0], [0., 0.], 1, 1) # test cope of knownPartODEfunc
   #print(test_call)
   n_ode(u0_network,p).+knownPartData
 end
@@ -54,22 +52,21 @@ end
 
 loss_n_ode(n_ode.p) # n_ode.p stores the initial parameters of the neural ODE
 
-cb = function (p,l,pred;doplot=false) #callback function to observe training
+cb = function (p, l, pred; doplot = false) #callback function to observe training
   display(l)
   # plot current prediction against data
   if doplot
-    pl = scatter(t,ode_data[1,:],label="data")
-    scatter!(pl,t,pred[1,:],label="prediction")
-    scatter!(t,ode_data[2,:],label="data")
-    scatter!(pl,t,pred[2,:],label="prediction")
+    pl = scatter(t,ode_data[1, :], label = "data")
+    scatter!(pl,t,pred[1, :], label = "prediction")
+    scatter!(t,ode_data[2, :], label = "data")
+    scatter!(pl,t,pred[2, :], label = "prediction")
     display(plot(pl))
   end
   return false
 end
 # Display the ODE with the initial parameter values.
-cb(n_ode.p,loss_n_ode(n_ode.p)...;doplot=true)
+cb(n_ode.p, loss_n_ode(n_ode.p)...; doplot = true)
 
 res1 = DiffEqFlux.sciml_train(loss_n_ode, n_ode.p, ADAM(0.01), cb = cb, maxiters = 500)
-cb(res1.minimizer,loss_n_ode(res1.minimizer)...;doplot=true)
-
+cb(res1.minimizer,loss_n_ode(res1.minimizer)...; doplot = true)
 #savefig("plots/test_result_hybrid_model4.pdf")
